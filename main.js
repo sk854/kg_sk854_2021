c2c81f67cbbba66dc34bef000dc8b68fa9b4e65b
Sooah Kang <sk854@cornell.edu>
2021-05-02 20:58:31 -0400

    console printing

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var print_numbers = function (num_array) {
   var nums = ['Zero', 'One', 'Two', 'Three', 'Four',
-    'Five', 'Six', 'Seven', 'Eight', 'Nine']
-  var result = ""
+    'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  var result = "";
   for (var i = 0; i < num_array.length; i++) {
     n_string = num_array[i].toString();
     for (var j = 0; j < n_string.length; j++) {
       n = parseInt(n_string.charAt(j));
-      result += nums[n]
+      result += nums[n];
     }
     if (i != num_array.length - 1) {
-      result += ", "
+      result += ", ";
     }
   }
   return result
 }
+
+num_array = process.argv.slice(2);
+console.log(print_numbers(num_array))
+
